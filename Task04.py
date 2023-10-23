a27bf71720eecd14e9e55e6e60594edf0b7d1a3c
Michael Ritchie <michael@ritchie.me>
2023-10-23 14:04:40 +0100

    T4 works with args

--- Task04.py
+++ Task04.py
@@ -1,51 +1,65 @@
 # Python file for Task 4 of the assignment - Rainbow Tables
+# Takes arguments for:
+# --hashesPath: Path to a hashes file
+# --tablePath: Path to a alreadt generated rainbow table
+# --dictionary: Path to the dictionary file for generating a new table
+# --chainLength: Length of each chain in the table
+# --chainCount: Number of chains in the table
+# --strLength: Length of the strings to be generated
+# --alphabet: Alphabet to be used to generate the strings
+
 
 import sys
+import argparse
 from cracking import Cracking
 
-t4_hashes = "./hashes/PasswordDictionary-hashes.txt"
-t4_table = "./rainbows/alphanum_cl10000_cc10000_sl8.rt"
-t4_dictionary = "./dictionaries/PasswordDictionary.txt"
+# Dictionary of defaults
+t4 = {
+  "hashesPath": "./hashes/PasswordDictionary-hashes.txt",
+  "tablePath": "None",
+  "dictionary": None,
+  "chainLength": 1000,
+  "chainCount": 1000,
+  "strLength": 8,
+  "alphabet": "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?#*$@-_."
+}
 
-def main(hashesPath=t4_hashes, rainbowPath=None, dictionaryPath=None, chainLength=None, chainCount=None, strLength=None, alphabet=None):
+def main():
   # If a rainbow table path exists, use it
-  if rainbowPath:
-    task = Cracking(hashesPath=hashesPath, rainbowTablePath=rainbowPath) # Create a new Cracking object with the hashes and table
+  if t4["tablePath"] != "None":
+    print(f"Using table: {t4['tablePath']}\nTo crack hashes: {t4['hashesPath']}")
+
+    task = Cracking(hashesPath=t4["hashesPath"], rainbowTablePath=t4["tablePath"]) # Create a new Cracking object with the hashes and table
     task.rainbowAttack() # Rainbow attack the hashes
   else: # Else, generate a new table
-    task = Cracking(hashesPath=hashesPath) # Create a new Cracking object with the hashes
-    task.rainbowAttack(newTable=True, chainLength=chainLength, chainCount=chainCount, strLength=strLength, alphabet=alphabet)
+    print(f"Generating table with dictionary: {t4['dictionary']}\nAlphabet: {t4['alphabet']}\nString length: {t4['strLength']} Chain length: {t4['chainLength']} Chain count: {t4['chainCount']}")
+    print(f"To crack hashes: {t4['hashesPath']}")
+
+    task = Cracking(hashesPath=t4["hashesPath"], dictionaryPath=t4["dictionary"]) # Create a new Cracking object with the hashes and potential dictionary
+    task.rainbowAttack(newTable=True, chainLength=t4["chainLength"], chainCount=t4["chainCount"], strLength=t4["strLength"], alphabet=t4["alphabet"]) # Generate a new table with the given parameters
   
   task.saveResults() # Save the results to a file
   print(task)
   return print(task.printInfo()) # Print the cracked hashes
 
 if __name__ == "__main__":
-    # Set input variables to defaults
-    input_hashes = t4_hashes
-    input_table = None
-    chainLength = 10000
-    chainCount = 100000
-    strLength = 8
-    alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  # Create parser
+  parser = argparse.ArgumentParser(description="Cracker: Rainbow table attack")
 
-    # Try to pass in a dictionary and hash array from the terminal
-    try:
-        if len(sys.argv) == 1: raise ValueError("No args passed in")
+  # Add arguments
+  parser.add_argument("--hashesPath", type=str, help="Password hash's file Path")
+  parser.add_argument("--tablePath", type=str, help="Rainbow table file path")
+  parser.add_argument("--dictionary", type=str, help="Dictionary file path")
+  parser.add_argument("--chainLength", type=int, help="Length of each chain in the table")
+  parser.add_argument("--chainCount", type=int, help="Number of chains in the table")
+  parser.add_argument("--strLength", type=int, help="Length of the strings to be generated")
+  parser.add_argument("--alphabet", type=str, help="Alphabet to be used to generate the strings")
 
-        # Try to use dictionary and hashes if passed in
-        input_hashes = sys.argv[1]
-        if len(sys.argv) > 2:
-          input_table = sys.argv[2]
+  # Parse arguments
+  args = parser.parse_args()
 
-    except ValueError as e:
-       print(f"Error: {e}, using default hashes and generating a table")
-    
-    finally:
-      if input_table:
-        print(f"Using hashes: {input_hashes} and table: {input_table}")
-        main(hashesPath=input_hashes, rainbowPath=input_table)
-      else: 
-        print(f"Using hashes: {input_hashes} and generating a table")
-        # Run main() with the dictionary and hashes
-        main(hashesPath=input_hashes, chainLength=chainLength, chainCount=chainCount, strLength=strLength, alphabet=alphabet)
+  # If an argument is given, update the dictionary
+  t4.update((key, value) for key, value in vars(args).items() if value != None)
+
+  # Try to run the main function
+  main()
